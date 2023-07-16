2b159c18c91c536fb380f405a7565bec21792368
dmitriy lazarev <web.dev.ya@ya.ru>
2023-07-17 00:24:28 +0300

    fix: joi errors

--- validation/rules.js
+++ validation/rules.js
@@ -2,33 +2,41 @@ const { Joi } = require('celebrate');
 const { urlRegex } = require('./regex');
 
 const stringRule = Joi.string().min(2).max(30);
-const urlRule = Joi.string().pattern(urlRegex);
+const stringRuleRequired = Joi.string().required().min(2).max(30);
+const urlRule = Joi.string().required().pattern(urlRegex);
 const idRule = Joi.string().required().hex().length(24);
 const emailRule = Joi.string().required().email();
 const pwdRule = Joi.string().required().min(1);
 
 const avatarObj = { avatar: urlRule };
-const userTextsObj = { name: stringRule, about: stringRule };
+const userTextsObj = { name: stringRuleRequired, about: stringRuleRequired };
 const loginObj = { email: emailRule, password: pwdRule };
 const cardObj = { name: stringRule, link: urlRule };
+const userObj = {
+  email: emailRule,
+  password: pwdRule,
+  name: stringRule,
+  about: stringRule,
+  avatar: urlRule,
+};
 
 const authRule = () => Joi.object().keys({
   autorization: Joi.string().replace('Bearer ', '').token(),
 });
 
-const userRule = () => Joi.object().keys({ ...loginObj, ...userTextsObj, ...avatarObj });
+const userRule = () => Joi.object().keys({ userObj });
 
-const loginRule = () => Joi.object().keys(loginObj);
+const loginRule = () => Joi.object().keys({ loginObj });
 
-const userTextsRule = () => Joi.object().keys(userTextsObj);
+const userTextsRule = () => Joi.object().keys({ userTextsObj });
 
-const avatarRule = () => Joi.object().keys(avatarObj);
+const avatarRule = () => Joi.object().keys({ avatarObj });
 
 const userIdRule = () => Joi.object().keys({ userId: idRule });
 
 const cardIdRule = () => Joi.object().keys({ cardId: idRule });
 
-const cardRule = () => Joi.object().keys(cardObj);
+const cardRule = () => Joi.object().keys({ cardObj });
 
 module.exports = {
   authRule, userRule, loginRule, cardIdRule, cardRule, userIdRule, userTextsRule, avatarRule,
