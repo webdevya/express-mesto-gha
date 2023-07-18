4a6a25abe5b8782bc8a66291ec5e76eaf239c58e
dmitriy lazarev <web.dev.ya@ya.ru>
2023-07-18 19:16:26 +0300

    reinstall eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "root": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": "airbnb-base",
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  }
+}
