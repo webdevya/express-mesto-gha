d95db39a48bd43c196f376dd6f3da9d95946aaeb
dmitriy lazarev <web.dev.ya@ya.ru>
2023-07-19 08:40:36 +0300

    added default values from to config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { PORT = 3000, DB_URL, JWT_SECRET } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb', JWT_SECRET = 'fffff71ac2582b2d853ff2afff136731a971798d24ebdd6eb80c9fe6adffffff' } = process.env;
 
 module.exports = { PORT, DB_URL, JWT_SECRET };
